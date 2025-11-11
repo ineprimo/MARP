5dccb7d465f32c6d658582a804ca1a324d88af49
(i)nés <ineprimo@ucm.es>
2025-11-11 10:15:42 +0100

    7.1

---
+++ 7/7-1/7-1.cpp
@@ -0,0 +1,73 @@
+
+/*@ <authors>
+ *
+ * Ines Primo Lopez MARP52
+ *
+ *@ </authors> */
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#include "DigrafoValorado.h"  // propios o los de las estructuras de datos de clase
+
+/*@ <answer>
+
+ Escribe aquí un comentario general sobre la solución, explicando cómo
+ se resuelve el problema y cuál es el coste de la solución, en función
+ del tamaño del problema.
+
+ @ </answer> */
+
+
+ // ================================================================
+ // Escribe el código completo de tu solución aquí debajo
+ // ================================================================
+ //@ <answer>
+
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	int V, A;
+	cin >> V >> A;
+
+	if (!std::cin)  // fin de la entrada
+		return false;
+
+	DigrafoValorado<int> grafo(V);
+
+	int a, b, j;
+
+	for (int i = 0; i < A; i++) {
+		//cin 
+	}
+
+	// resolver el caso posiblemente llamando a otras funciones
+
+	// escribir la solución
+
+	return true;
+}
+
+//@ </answer>
+//  Lo que se escriba dejado de esta línea ya no forma parte de la solución.
+
+int main() {
+	// ajustes para que cin extraiga directamente de un fichero
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	if (!in.is_open())
+		std::cout << "Error: no se ha podido abrir el archivo de entrada." << std::endl;
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+
+	// para dejar todo como estaba al principio
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	std::cout << "Pulsa Intro para salir..." << std::flush;
+	std::cin.get();
+#endif
+	return 0;
+}
